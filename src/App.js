88cf3b98e9be0f215269dbea977edab3c1a34439
Vinicius Moraes <vinicius.oliveira@smartmei.com.br>
2020-03-04 16:23:04 -0300

    add class Tabela

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import Tabela from './Tabela';
+
+class App extends Component {
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000',
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99',
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150',
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100',
+      },
+      {
+        nome: 'Nico',
+        livro: 'Java',
+        preco: '9999',
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores = { this.state.autores }/>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
