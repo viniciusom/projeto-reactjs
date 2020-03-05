c15d3e3ce94a345871549ad43c17c5e75f5c5eb7
Vinicius Moraes <vinicius.oliveira@smartmei.com.br>
2020-03-05 11:52:49 -0300

    Adicionado metodo de remoção com state

--- src/App.js
+++ src/App.js
@@ -32,10 +32,23 @@ class App extends Component {
       },
     ],
   };
+
+  removeAutor = index => {
+    const { autores } = this.state;
+    this.setState(
+      {
+        autores: autores.filter((autor, posAtual) => {
+          console.log(index, posAtual);
+          return posAtual !== index;
+        }),
+      },
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <Tabela autores = { this.state.autores }/>
+        <Tabela autores = { this.state.autores } removeAutor = { this.removeAutor } />
       </div>
     );
   }
